cdd492a9de4740d3d4c6f6e79e112fec75839e5f
rebelkov <antoinevokleber@gmail.com>
2016-12-29 09:08:23 +0100

    dev purge img

--- scenes/menu.lua
+++ scenes/menu.lua
@@ -58,8 +58,21 @@ function scene:create( event )
     -- setup a page background, really not that important though composer
     -- crashes out if there isn't a display object in the view.
     --
-    
-    
+    -- local background=display.newImage("images/fourmi_background.png")
+    --  sceneGroup:insert( background )
+
+local _W, _H, _CX, _CY = relayout._W, relayout._H, relayout._CX, relayout._CY
+
+   
+
+    local background = display.newRect(sceneGroup, _CX, _CY, _W, _H)
+    background.fill = {
+        type = 'gradient',
+        color1 = {0.2, 0.45, 0.8},
+        color2 = {0.7, 0.8, 1}
+    }
+    relayout.add(background)
+
 
     local title = display.newText("ANTLOST", 100, 32, native.systemFontBold, 64 )
     title.x = display.contentCenterX
